ab87874c160dd1809780148f0845b9b08640fce2
okoeee <yk27.puxcm@gmail.com>
2023-09-10 11:30:52 +0900

    不要なimportを削除

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,27 +3,19 @@ package controllers
 import persistence.repository.TodoRepository
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(todoRepository: TodoRepository, val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(
+  todoRepository: TodoRepository,
+  val controllerComponents: ControllerComponents
+) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index() = Action { implicit req: Request[AnyContent] =>
     todoRepository.all.map(todos => println(todos))
     Ok(views.html.index())
   }
+
 }
