4e1e676bef50e07496c1a05abcadd507edd4a527
okoeee <yk27.puxcm@gmail.com>
2023-09-12 09:42:30 +0900

    TodoStatusクラスを作成

--- app/domain/model/todo/Todo.scala
+++ app/domain/model/todo/Todo.scala
@@ -8,3 +8,13 @@ case class Todo(
   body: String,
   state: Int
 )
+
+sealed abstract class TodoStatus(
+  val code: Short,
+  val name: String
+)
+object TodoStatus {
+  case object NOT_STARTED extends TodoStatus(code = 0, name = "未着手")
+  case object IN_PROGRESS extends TodoStatus(code = 1, name = "進行中")
+  case object COMPLETED extends TodoStatus(code = 2, name = "完了")
+}
