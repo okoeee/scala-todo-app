2ec062e3dc07e142a853e8cad39327b8b0ac61e8
okoeee <yk27.puxcm@gmail.com>
2023-09-17 22:47:59 +0900

    update TodoControllerSpec

--- test/controllers/TodoControllerSpec.scala
+++ test/controllers/TodoControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.libs.json.Json
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -10,12 +11,27 @@ class TodoControllerSpec
   with GuiceOneAppPerTest
   with Injecting {
 
-  "TodoController GET" must {
-    "return json from the application" in {
-      val request = FakeRequest(GET, "/")
+  "GET /todo" must {
+    "return json" in {
+      val request = FakeRequest(GET, "/todo")
       val response = route(app, request).get
 
       status(response) mustBe OK
+      contentType(response) mustBe Some("application/json")
+    }
+  }
+
+  "POST /todo" must {
+    "return error json" in {
+      val request = FakeRequest(POST, "/todo")
+        .withJsonBody(
+          Json.obj(
+            "categoryId" -> "0"
+          )
+        )
+      val response = route(app, request).get
+
+      status(response) mustBe 400
       contentType(response) mustBe Some("application/json")
     }
   }
