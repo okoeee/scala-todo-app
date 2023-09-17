193647059302aac842863790ad05203a1d2a3d81
okoeee <yk27.puxcm@gmail.com>
2023-09-17 22:25:03 +0900

    add post action to TodoController

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -1,13 +1,14 @@
 package controllers
 
-import adapter.json.JsValueTodo
+import adapter.json._
 import domain.repository.TodoRepository
-import play.api.libs.json.Json
+import play.api.libs.json.{JsError, JsValue, Json}
 
 import javax.inject._
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class TodoController @Inject() (
@@ -18,10 +19,25 @@ class TodoController @Inject() (
   def index(): Action[AnyContent] = Action.async { implicit req =>
     for {
       todoList <- todoRepository.all
-      jsValueTodoList = todoList.map(todo => JsValueTodo(todo))
+      jsValueTodoList = todoList.map(todo => writes.JsValueTodo(todo))
     } yield {
       Ok(Json.toJson(jsValueTodoList))
     }
+  }
+
+  def post: Action[JsValue] = Action.async(parse.json) { implicit req =>
+    req.body
+      .validate[reads.JsValueTodo]
+      .fold(
+        errors => {
+          val errorMsg = JsError.toJson(errors).toString
+          Future.successful(BadRequest(Json.obj("message" -> errorMsg)))
+        },
+        todoData => {
+          println(todoData)
+          Future.successful(Ok(Json.obj("message" -> "success")))
+        }
+      )
   }
 
 }
