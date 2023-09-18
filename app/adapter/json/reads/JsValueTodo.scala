46d69fbb1f2554720fa086b46ce9a2a7cb517157
okoeee <yk27.puxcm@gmail.com>
2023-09-18 14:24:28 +0900

    update JsValue

--- app/adapter/json/reads/JsValueTodo.scala
+++ app/adapter/json/reads/JsValueTodo.scala
@@ -1,5 +1,6 @@
 package adapter.json.reads
 
+import domain.model.todo.{Todo, TodoStatus}
 import play.api.libs.json.{Json, Reads}
 
 case class JsValueTodo(
@@ -10,4 +11,14 @@ case class JsValueTodo(
 )
 object JsValueTodo {
   implicit val reads: Reads[JsValueTodo] = Json.reads[JsValueTodo]
+
+  def toTodo(jsValueTodo: JsValueTodo) =
+    Todo(
+      id = 0,
+      categoryId = jsValueTodo.categoryId,
+      title = jsValueTodo.title,
+      body = jsValueTodo.body,
+      state = TodoStatus.fromShort(jsValueTodo.state)
+    )
+
 }
