dc41746c76fbc0864db5a828f3a0f33af114507a
okoeee <yk27.puxcm@gmail.com>
2023-09-19 21:42:33 +0900

    update sql

--- docker/db/sql/01_create_table.sql
+++ docker/db/sql/01_create_table.sql
@@ -1,14 +1,28 @@
+CREATE TABLE `user` (
+  `id`         BIGINT       unsigned NOT NULL AUTO_INCREMENT,
+  `name`       VARCHAR(255) NOT NULL,
+  `email`      VARCHAR(255) NOT NULL,
+  `password`   VARCHAR(255) NOT NULL,
+  `updated_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `key01` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `category` (
   `id`         BIGINT       unsigned NOT NULL AUTO_INCREMENT,
+  `user_id`    BIGINT       unsigned NOT NULL,
   `name`       VARCHAR(255) NOT NULL,
   `color`      TINYINT      unsigned  NOT NULL,
   `updated_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES user(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `todo` (
   `id`          BIGINT       unsigned NOT NULL AUTO_INCREMENT,
+  `user_id`     BIGINT       unsigned NOT NULL,
   `category_id` BIGINT       unsigned,
   `title`       VARCHAR(255) NOT NULL,
   `body`        TEXT,
@@ -16,5 +30,6 @@ CREATE TABLE `todo` (
   `updated_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES user(`id`),
   FOREIGN KEY (`category_id`) REFERENCES category(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- docker/db/sql/02_insert_initial_data.sql
+++ docker/db/sql/02_insert_initial_data.sql
@@ -1,5 +1,7 @@
-INSERT INTO category(name, color) VALUES ('フロントエンド', 1);
-INSERT INTO category(name, color) VALUES ('バックエンド', 2);
+INSERT INTO user(id, name, email, password) VALUES (1, 'taro', 'taro@taro.com', 'aaaa');
 
-INSERT INTO todo(category_id, title, body, state) VALUES (1, 'UI調節', 'ヘッダーの位置調節', 1);
-INSERT INTO todo(category_id, title, body, state) VALUES (2, 'API', 'Todoを返すAPIを作成', 2);
+INSERT INTO category(user_id, name, color) VALUES (1, 'フロントエンド', 1);
+INSERT INTO category(user_id, name, color) VALUES (1, 'バックエンド', 2);
+
+INSERT INTO todo(user_id, category_id, title, body, state) VALUES (1, 1, 'UI調節', 'ヘッダーの位置調節', 1);
+INSERT INTO todo(user_id, category_id, title, body, state) VALUES (1, 2, 'API', 'Todoを返すAPIを作成', 2);
