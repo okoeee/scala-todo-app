a7081ea2d93af4cce45bd732d73ed0c81372d602
okoeee <yk27.puxcm@gmail.com>
2023-09-22 22:14:58 +0900

    add JsValueLogin

---
+++ app/adapter/json/reads/JsValueLogin.scala
@@ -0,0 +1,11 @@
+package adapter.json.reads
+
+import play.api.libs.json.{Json, Reads}
+
+case class JsValueLogin(
+  email:    String,
+  password: String
+)
+object JsValueLogin {
+  implicit val reads: Reads[JsValueLogin] = Json.reads[JsValueLogin]
+}
