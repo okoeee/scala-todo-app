11851d0529d9e2237e97beb94e18adb1150d5b0b
okoeee <yk27.puxcm@gmail.com>
2023-09-22 22:48:20 +0900

    update module

--- app/Module.scala
+++ app/Module.scala
@@ -1,9 +1,10 @@
 import com.google.inject.AbstractModule
-import domain.repository.TodoRepository
-import infrastructure.repositoryimpl.TodoRepositoryImpl
+import domain.repository.{TodoRepository, UserRepository}
+import infrastructure.repositoryimpl.{TodoRepositoryImpl, UserRepositoryImpl}
 
 class Module extends AbstractModule {
   override def configure(): Unit = {
+    bind(classOf[UserRepository]).to(classOf[UserRepositoryImpl])
     bind(classOf[TodoRepository]).to(classOf[TodoRepositoryImpl])
   }
 }
