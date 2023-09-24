56f6bb7933f2bc5a673642e5d819c956dfac995b
okoeee <yk27.puxcm@gmail.com>
2023-09-24 22:33:38 +0900

    update AuthController

--- app/adapter/controllers/AuthController.scala
+++ app/adapter/controllers/AuthController.scala
@@ -2,11 +2,13 @@ package adapter.controllers
 
 import adapter.controllers.helpers.FormHelper
 import adapter.controllers.mvc.ImplicitConverter
+import adapter.controllers.mvc.action.AuthenticatedAction
 import adapter.json.reads.JsValueLogin
 import cats.data.EitherT
 import domain.service.UserSessionCommandService
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, BaseController, ControllerComponents}
+import play.filters.csrf.CSRF
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.Future
@@ -14,10 +16,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class AuthController @Inject() (
+  authenticatedAction:       AuthenticatedAction,
   userSessionCommandService: UserSessionCommandService,
   val controllerComponents:  ControllerComponents
 ) extends BaseController
   with ImplicitConverter {
+
+  def verify() = authenticatedAction.async { implicit req =>
+    Future(Ok)
+  }
 
   def login(): Action[JsValue] = Action.async(parse.json) { implicit req =>
     val param = for {
@@ -28,7 +35,7 @@ class AuthController @Inject() (
       (for {
         token <- userSessionCommandService.login(email, password)
       } yield Ok(Json.obj("message" -> "success"))
-        .withSession("sid" -> token.toString)) recover {
+        .withSession("token" -> token.value)) recover {
         case _: NoSuchElementException =>
           Ok(Json.obj("message" -> "no user exists"))
       }
