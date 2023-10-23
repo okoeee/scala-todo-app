97b84ac8fdbfbbc7ed7b567182aa291814ca0e8a
okoeee <yk27.puxcm@gmail.com>
2023-10-23 09:00:44 +0900

    remove todo comment

--- app/domain/model/user/Password.scala
+++ app/domain/model/user/Password.scala
@@ -7,7 +7,6 @@ import com.github.t3hnar.bcrypt._
 case class Password(
   value: String
 ) extends MappedTo[String] {
-  // todo PasswordにはUserIdの情報をもたせ、別テーブルで情報を管理したほうが良さそう
   def encrypt: Password =
     Password(this.value.bcryptSafeBounded.get)
 }
