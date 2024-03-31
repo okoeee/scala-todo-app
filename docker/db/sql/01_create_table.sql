0657aeb8b6319b420f02d2bdc1b0f2d85b52f919
okoeee <yk27.puxcm@gmail.com>
2024-03-31 21:26:09 +0900

    update sql

--- docker/db/sql/01_create_table.sql
+++ docker/db/sql/01_create_table.sql
@@ -1,3 +1,21 @@
+
+CREATE TABLE `group` (
+  `id`         BIGINT       unsigned NOT NULL AUTO_INCREMENT,
+  `name`       VARCHAR(255) NOT NULL,
+  `updated_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `group_membership` (
+  `group_id`   BIGINT       NOT NULL,
+  `user_id`    BIGINT       NOT NULL,
+  `updated_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX `key01` (`group_id`),
+  INDEX `key02` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `user` (
   `id`         BIGINT       unsigned NOT NULL AUTO_INCREMENT,
   `name`       VARCHAR(255) NOT NULL,
@@ -23,25 +41,23 @@ CREATE TABLE `user_session` (
 
 CREATE TABLE `category` (
   `id`         BIGINT       unsigned NOT NULL AUTO_INCREMENT,
-  `user_id`    BIGINT       unsigned NOT NULL,
+  `group_id`   BIGINT       unsigned NOT NULL,
   `name`       VARCHAR(255) NOT NULL,
   `color`      TINYINT      unsigned  NOT NULL,
   `updated_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES user(`id`)
+  INDEX `key01` (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `todo` (
   `id`          BIGINT       unsigned NOT NULL AUTO_INCREMENT,
-  `user_id`     BIGINT       unsigned NOT NULL,
+  `group_id`    BIGINT       unsigned NOT NULL,
   `category_id` BIGINT       unsigned,
   `title`       VARCHAR(255) NOT NULL,
   `body`        TEXT,
   `state`       TINYINT      unsigned NOT NULL,
   `updated_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`user_id`) REFERENCES user(`id`),
-  FOREIGN KEY (`category_id`) REFERENCES category(`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- docker/db/sql/02_insert_initial_data.sql
+++ docker/db/sql/02_insert_initial_data.sql
@@ -1,7 +1,23 @@
-INSERT INTO user(id, name, email, password) VALUES (1, 'taro', 'taro@taro.com', '$2a$10$thTkcDVr1hXw8ryvEZqP4eAt5SdxxYL9ERCea0jztQSV5G707/FRW');
+-- group
+INSERT INTO `group` (id, name)
+VALUES (1, 'テストグループ');
 
-INSERT INTO category(user_id, name, color) VALUES (1, 'フロントエンド', 1);
-INSERT INTO category(user_id, name, color) VALUES (1, 'バックエンド', 2);
+-- group_membership
+INSERT INTO `group_membership` (group_id, user_id)
+VALUES (1, 1);
 
-INSERT INTO todo(user_id, category_id, title, body, state) VALUES (1, 1, 'UI調節', 'ヘッダーの位置調節', 1);
-INSERT INTO todo(user_id, category_id, title, body, state) VALUES (1, 2, 'API', 'Todoを返すAPIを作成', 2);
+-- user
+INSERT INTO `user` (id, name, email, password)
+VALUES (1, 'taro', 'taro@taro.com', '$2a$10$thTkcDVr1hXw8ryvEZqP4eAt5SdxxYL9ERCea0jztQSV5G707/FRW');
+
+-- todo
+INSERT INTO `category` (group_id, name, color)
+VALUES (1, 'フロントエンド', 1);
+INSERT INTO `category` (group_id, name, color)
+VALUES (1, 'バックエンド', 2);
+
+-- category
+INSERT INTO `todo` (user_id, category_id, title, body, state)
+VALUES (1, 1, 'UI調節', 'ヘッダーの位置調節', 1);
+INSERT INTO `todo` (user_id, category_id, title, body, state)
+VALUES (1, 2, 'API', 'Todoを返すAPIを作成', 2);
