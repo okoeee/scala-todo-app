0b696ebea8d240869206e9f01aa238fcc5da1bca
okoeee <yk27.puxcm@gmail.com>
2024-04-14 21:50:11 +0900

    add group membership di

--- app/Module.scala
+++ app/Module.scala
@@ -1,11 +1,13 @@
 import com.google.inject.AbstractModule
 import domain.repository.{
+  GroupMembershipRepository,
   GroupRepository,
   TodoRepository,
   UserRepository,
   UserSessionRepository
 }
 import infrastructure.repositoryimpl.{
+  GroupMembershipImpl,
   GroupRepositoryImpl,
   TodoRepositoryImpl,
   UserRepositoryImpl,
@@ -15,6 +17,7 @@ import infrastructure.repositoryimpl.{
 class Module extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[GroupRepository]).to(classOf[GroupRepositoryImpl])
+    bind(classOf[GroupMembershipRepository]).to(classOf[GroupMembershipImpl])
     bind(classOf[UserRepository]).to(classOf[UserRepositoryImpl])
     bind(classOf[UserSessionRepository]).to(classOf[UserSessionRepositoryImpl])
     bind(classOf[TodoRepository]).to(classOf[TodoRepositoryImpl])
