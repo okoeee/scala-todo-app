50d505b2f56c92350969239ef487aaefd385b0ad
okoeee <yk27.puxcm@gmail.com>
2024-04-28 22:42:05 +0900

    add groupId to cookie

--- app/adapter/controllers/AuthController.scala
+++ app/adapter/controllers/AuthController.scala
@@ -6,9 +6,21 @@ import adapter.controllers.mvc.action.AuthenticatedAction
 import adapter.json.reads.JsValueLogin
 import adapter.json.writes.JsValueUserState
 import cats.data.EitherT
+import domain.model.user.Password
+import domain.repository.{
+  GroupMembershipRepository,
+  GroupRepository,
+  UserRepository
+}
 import domain.service.UserSessionCommandService
 import play.api.libs.json.{JsValue, Json}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import play.api.mvc.{
+  Action,
+  AnyContent,
+  BaseController,
+  ControllerComponents,
+  Cookie
+}
 import play.filters.csrf.CSRF
 
 import javax.inject.{Inject, Singleton}
@@ -19,6 +31,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class AuthController @Inject() (
   authenticatedAction:       AuthenticatedAction,
   userSessionCommandService: UserSessionCommandService,
+  userRepository:            UserRepository,
+  groupRepository:           GroupRepository,
+  groupMembershipRepository: GroupMembershipRepository,
   val controllerComponents:  ControllerComponents
 ) extends BaseController
   with ImplicitConverter {
@@ -39,12 +54,17 @@ class AuthController @Inject() (
     } yield (jsValueLogin.email, jsValueLogin.password)
 
     EitherT.fromEither[Future](param) semiflatMap { case (email, password) =>
-      (for {
+      for {
         token <- userSessionCommandService.login(email, password)
-      } yield Ok(Json.obj("message" -> "success"))
-        .withSession("token" -> token.value)) recover {
-        case _: NoSuchElementException =>
-          Ok(Json.obj("message" -> "no user exists"))
+        Some(user) <-
+          userRepository.findByEmailAndPassword(email, Password(password))
+        Some(groupMembership) <- groupMembershipRepository.findByUserId(user.id)
+        Some(group) <- groupRepository.findById(groupMembership.groupId)
+      } yield {
+        val cookie = Cookie("groupId", group.id.toString)
+        Ok(Json.obj("message" -> "success"))
+          .withSession("token" -> token.value)
+          .withCookies(cookie)
       }
     }
   }
