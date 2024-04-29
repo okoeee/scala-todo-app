84e7bf50ba21ee1d6465262754d0425659e67bc4
okoeee <yk27.puxcm@gmail.com>
2024-04-29 10:10:38 +0900

    modify comment

--- docker/db/sql/02_insert_initial_data.sql
+++ docker/db/sql/02_insert_initial_data.sql
@@ -10,13 +10,13 @@ VALUES (1, 1);
 INSERT INTO `user` (id, name, email, password)
 VALUES (1, 'taro', 'taro@taro.com', '$2a$10$thTkcDVr1hXw8ryvEZqP4eAt5SdxxYL9ERCea0jztQSV5G707/FRW');
 
--- todo
+-- category
 INSERT INTO `category` (group_id, name, color)
 VALUES (1, 'フロントエンド', 1);
 INSERT INTO `category` (group_id, name, color)
 VALUES (1, 'バックエンド', 2);
 
--- category
+-- todo
 INSERT INTO `todo` (user_id, category_id, title, body, state)
 VALUES (1, 1, 'UI調節', 'ヘッダーの位置調節', 1);
 INSERT INTO `todo` (user_id, category_id, title, body, state)
